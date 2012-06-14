0028fc2693b481539c8bf3bca75fbf676b1448df
shashank singh <shashank.personal@gmail.com>
2012-06-15 03:48:12 +0530

    following http://www.youtube.com/watch?feature=player_embedded&v=WuiHuZq_cg4#! ad added some snazzy text effects

--- experimentTwo/todo.js
+++ experimentTwo/todo.js
@@ -1,3 +1,7 @@
 function TodoCtrl($scope) {
     $scope.totalTodo = 4;
+    $scope.todos = [
+                    {text:"learn",done:true},
+                    {text:"Dont Learn",done:false}
+    ];
 }
